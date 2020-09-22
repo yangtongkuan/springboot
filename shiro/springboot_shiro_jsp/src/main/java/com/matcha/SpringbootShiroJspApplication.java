bb8d4cf95c98222d6e7880a9112ce69f8e58e8c3
yangtongkuan <yangtongkuan@qq.com>
2020-09-22 23:49:11 +0800

    shiro 整合 jsp

---
+++ shiro/springboot_shiro_jsp/src/main/java/com/matcha/SpringbootShiroJspApplication.java
@@ -0,0 +1,13 @@
+package com.matcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShiroJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShiroJspApplication.class, args);
+    }
+
+}
